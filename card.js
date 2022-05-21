64c664cdaba2bd7186468d21665e329b51caaf31
nilesfromm <nsfromm@gmail.com>
2022-05-21 23:29:35 +0200

    updated info points
    Fixes #4

--- card.js
+++ card.js
@@ -30,7 +30,35 @@ class Card {
 
 			cardRoughness:			1,
 			cardMetalness:			0,
+
+			infoDebug:				false,
 		}
+
+		this.tempPt = new THREE.Vector3();
+
+		this.infoPoints = [
+			{
+				name:	'point1',
+				div: 	document.getElementById('point1'),
+				x:		0.24,
+				y:		0.39,
+				pt:		undefined,
+			},
+			{
+				name:	'point2',
+				div: 	document.getElementById('point2'),
+				x:		0.88,
+				y:		-0.62,
+				pt:		undefined,
+			},
+			{
+				name:	'point3',
+				div: 	document.getElementById('point3'),
+				x:		-0.62,
+				y:		0.01,
+				pt:		undefined,
+			}
+		]
 
 		this.cardMat = new THREE.MeshPhysicalMaterial({
 			color:			this.settings.borderColor,
@@ -47,11 +75,14 @@ class Card {
 			map: loader.load('assets/image/card_test.jpg'),
 		})
 
+		this.infoMat = new THREE.MeshStandardMaterial({ color: 'coral', visible: this.settings.infoDebug });
+
 		//-----------------------------------------------------------------------
 		//setup
 		this.gui_folder = _G.DATGUI.addFolder("CARD");
 		this.gui_rotation = this.gui_folder.addFolder("ROTATION");
 		this.gui_materials = this.gui_folder.addFolder("MATERIALS");
+		this.gui_infoPts = this.gui_folder.addFolder("INFO PTS");
 		// this.globalSetup();
 		this.setup_gui();
 		// this.init_interactions();
@@ -68,6 +99,24 @@ class Card {
 		});
 	}
 
+	init_infoPt(_parent, _i, _x, _y, _name){
+		//card dims(x,y) -> (3.2,4.45)
+		const geometry = new THREE.SphereGeometry( 0.1, 16, 32 );
+		this.infoPoints[_i].pt = new THREE.Mesh(geometry, this.infoMat)
+		this.infoPoints[_i].pt.position.x = _x * 3.2 / 2;
+		this.infoPoints[_i].pt.position.y = _y * 4.45 / 2;
+		// this.videoPlane.scale.set(this.settings.videoScale,this.settings.videoScale,this.settings.videoScale);
+		_parent.add(this.infoPoints[_i].pt);
+
+		let ptGui = this.gui_infoPts.addFolder(_name);
+		ptGui.add(this.infoPoints[_i], "x", -1, 1, 0.01).onChange((val)=>{
+			this.infoPoints[_i].pt.position.x = val * 3.2 / 2;
+		})
+		ptGui.add(this.infoPoints[_i], "y", -1, 1, 0.01).onChange((val)=>{
+			this.infoPoints[_i].pt.position.y = val * 4.45 / 2;
+		})
+	}
+
 	//=========================================================================================================
 	init_interactions(){
 		//add camera move code here
@@ -76,10 +125,6 @@ class Card {
 			this.settings.my = (e.clientY / window.innerHeight - 0.5) * 2;
 
 			this.settings.move_target.set(this.settings.mx,this.settings.my,0);
-			
-			// console.log(x, y);
-			// this.mesh.rotation.x = y * this.settings.YrotateFactor;
-			// this.mesh.rotation.y = x * this.settings.XrotateFactor;
 		}
 	}
 
@@ -107,7 +152,12 @@ class Card {
 					if (child.name.includes("image")){
                         child.material = this.imageMat;
 						child.material.map.flipY = false;
-						console.log(child.material);
+						console.log("card",child);
+
+						this.infoPoints.forEach((pt, i)=>{
+							this.init_infoPt(child, i, pt.x, pt.y, pt.name);
+						})
+						
                     }
 				}
 			});
@@ -127,9 +177,27 @@ class Card {
 
 		if(!this.updating){return;}
 		this.update_counter++;
+
 		this.settings.move_value.lerp(this.settings.move_target,this.settings.lerp);
 		this.mesh.rotation.x = this.settings.move_value.y * this.settings.YrotateFactor;
 		this.mesh.rotation.y = this.settings.move_value.x * this.settings.XrotateFactor;
+
+		this.infoPoints.forEach((pt, i)=>{
+
+			pt.pt.getWorldPosition(this.tempPt);
+
+			// map to normalized device coordinate (NDC) space
+			this.tempPt.project( _G.MYSCENE.camera );
+
+			// map to 2D screen space
+			this.tempPt.x = Math.round( (   this.tempPt.x + 1 ) * _G.MYSCENE.container.width  / 2 );
+			this.tempPt.y = Math.round( ( - this.tempPt.y + 1 ) * _G.MYSCENE.container.height / 2 );
+			this.tempPt.z = 0;
+
+			pt.div.style.top = this.tempPt.y - 25 + "px";
+			pt.div.style.left = this.tempPt.x - 25 + "px";
+
+		})
 
 	}
 
@@ -161,6 +229,10 @@ class Card {
 		this.gui_materials.add(this.settings, "cardMetalness", 0, 1, 0.01).onChange((val)=>{
 			this.imageMat.metalness = val;
 		});
+
+		this.gui_infoPts.add(this.settings, "infoDebug").onChange((val)=>{
+			this.infoMat.visible = val;
+		})
 	}
 
 	//--------------------------------------------------------------------------
